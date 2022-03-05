954bb6175aef10ee620b1efe9618ed34f783ef82
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-05 10:24:18 -0800

    built out static file, and two routes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use warp::Filter;
 
+const WEB_FOLDER: &str = "web/";
+
 #[tokio::main]
 async fn main() {
-    let welcome_route = warp::path::end().map(|| "Hello, world!");
+    // API endpoints
+    let welcome_route = warp::path::end()
+        .and(warp::get())
+        .map(|| "Hello, world!");
+
+    let hi = warp::path("hi").and(warp::get())
+        .map(|| "Hi!");
+
+        
+    // Static
+    let content = warp::fs::dir(WEB_FOLDER);
+    let root = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(format!("{}index.html", WEB_FOLDER)));
+        
+    let static_files = content.or(root);
+        
+    let routes = welcome_route.or(hi).or(static_files);
 
     println!("starting web server");
-    warp::serve(welcome_route).run(([127, 0, 0, 1], 3001)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], 3001)).await;
 }
